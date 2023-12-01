5f431b641177099950b09c3f14ca4244a1605846
松岡 利昌 <t.matsuoka@kobesoft.co.jp>
2023-12-01 18:57:14 +0900

    Update Module.php

--- src/Module.php
+++ src/Module.php
@@ -2,6 +2,7 @@
 
 namespace Green\ResourceModule;
 
+use Filament\Actions\ActionGroup;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Form;
 use Filament\Tables\Actions\Action;
@@ -44,26 +45,31 @@ class Module
      * @param array $insert 挿入するコンポーネントの配列
      * @return array 結果のコンポーネントの配列
      */
-    private static function insertComponents(array $array, ?string $before, array $insert): array
+    private static function insertComponents(array $array, array $insert, ?string $before, ?string $after): array
     {
+        if (($before !== null) == ($after !== null)) {
+            throw new RuntimeException('Either before or after must be specified.');
+        }
+        $name = $before ?? $after;
+        $offset = $before !== null ? 0 : 1;
         foreach (array_values($array) as $i => $component) {
             // ActionGroupの場合の再帰的な処理
-            if ($component instanceof \Filament\Actions\ActionGroup) {
+            if ($component instanceof ActionGroup) {
                 if ($children = $component->getActions()) {
-                    $component->actions(self::insertComponents($children, $before, $insert));
+                    $component->actions(self::insertComponents($children, $insert, $before, $after));
                 }
             }
 
             // Componentの場合の再帰的な処理
             if ($component instanceof Component) {
                 if ($children = $component->getChildComponents()) {
-                    $component->childComponents(self::insertComponents($children, $before, $insert));
+                    $component->childComponents(self::insertComponents($children, $insert, $before, $after));
                 }
             }
 
             // 挿入位置のコンポーネントの前に挿入する
-            if (method_exists($component, 'getName') && $component->getName() == $before) {
-                array_splice($array, $i, 0, $insert);
+            if (method_exists($component, 'getName') && $component->getName() == $name) {
+                array_splice($array, $i + $offset, 0, $insert);
                 return $array;
             }
         }
@@ -74,51 +80,55 @@ class Module
      * テーブルのカラムにコンポーネントを追加する
      *
      * @param Table $table 追加する対象のテーブル
-     * @param string $before 挿入位置のカラム名
      * @param Column[] $columns 追加するカラムの配列
+     * @param string|null $before 挿入位置のカラム名
+     * @param string|null $after
      * @return Table 追加後のテーブル
      */
-    public static function addTableColumns(Table $table, string $before, array $columns): Table
+    public static function addTableColumns(Table $table, array $columns, string $before = null, string $after = null): Table
     {
-        return $table->columns(self::insertComponents($table->getColumns(), $before, $columns));
+        return $table->columns(self::insertComponents($table->getColumns(), $columns, $before, $after));
     }
 
     /**
      * テーブルにフィルターを追加する
      *
      * @param Table $table 追加する対象のテーブル
-     * @param string $before 挿入位置のフィルター名
      * @param array $filters 追加するフィルターの配列
+     * @param string|null $before 挿入位置のフィルター名
+     * @param string|null $after
      * @return Table 追加後のテーブル
      */
-    public static function addTableFilters(Table $table, string $before, array $filters): Table
+    public static function addTableFilters(Table $table, array $filters, string $before = null, string $after = null): Table
     {
-        return $table->filters(self::insertComponents($table->getFilters(), $before, $filters));
+        return $table->filters(self::insertComponents($table->getFilters(), $filters, $before, $after));
     }
 
     /**
      * テーブルにアクションを追加する
      *
      * @param Table $table 追加する対象のテーブル
-     * @param string $before 挿入位置のアクション名
      * @param Action[] $actions 追加するアクションの配列
+     * @param string|null $before 挿入位置のアクション名
+     * @param string|null $after
      * @return Table 追加後のテーブル
      */
-    public static function addTableActions(Table $table, string $before, array $actions): Table
+    public static function addTableActions(Table $table, array $actions, string $before = null, string $after = null): Table
     {
-        return $table->actions(self::insertComponents($table->getActions(), $before, $actions));
+        return $table->actions(self::insertComponents($table->getActions(), $actions, $before, $after));
     }
 
     /**
      * フォームにコンポーネントを追加する
      *
      * @param Form $form 追加する対象のフォーム
-     * @param string $before 挿入位置のコンポーネント名
      * @param Component[] $components 追加するコンポーネントの配列
+     * @param string|null $before 挿入位置のコンポーネント名
+     * @param string|null $after
      * @return Form 追加後のフォームかテーブル
      */
-    public static function addFormSchema(Form $form, string $before, array $components): Form
+    public static function addFormSchema(Form $form, array $components, string $before = null, string $after = null): Form
     {
-        return $form->components(self::insertComponents($form->getComponents(true), $before, $components));
+        return $form->components(self::insertComponents($form->getComponents(true), $components, $before, $after));
     }
 }
